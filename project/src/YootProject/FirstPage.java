dd75a894c96eaeba2493d3d55f883cff79f485a7
brian <kimchanjoong54@gmail.com>
2025-05-03 15:53:35 +0900

    java 프로젝트 파일 생성, 및 first page 생성

---
+++ project/src/YootProject/FirstPage.java
@@ -0,0 +1,17 @@
+package YootProject;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class FirstPage extends JFrame{
+    private static final int Max_Player = 4; // 최대 인원 수
+    private static final int Max_Piece = 5; // 최대 말의 개수
+
+
+
+    
+    public static void main(String[] args) {
+        new FirstPage();
+    }
+}
