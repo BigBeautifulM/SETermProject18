29b54548754a5ec2cfe99c3a4d951af1b07123d6
BigBeautifulM <67548281+BigBeautifulM@users.noreply.github.com>
2025-05-29 13:20:59 +0900

    UI- Completion

---
+++ projectjavafx/srcjavafx/YootProjectjavafx/EndPagefx.java
@@ -0,0 +1,22 @@
+package YootProjectjavafx;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EndPagefx {
+    public EndPagefx(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("EndPage"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ projectjavafx/srcjavafx/YootProjectjavafx/EndPagefxController.java
@@ -0,0 +1,29 @@
+package YootProjectjavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class EndPagefxController {
+    private EndPagefx page;
+
+    @FXML
+    private AnchorPane endPage;
+
+    public EndPagefxController(EndPagefx endpage){this.page = endpage;}
+
+    @FXML
+    private void handleRestart(ActionEvent e){
+        Stage stage = (Stage) endPage.getScene().getWindow();
+        stage.close();
+        new FirstPagefx();
+    }
+
+    @FXML
+    private void handleExit(ActionEvent e){
+        System.exit(0);
+    }
+}
